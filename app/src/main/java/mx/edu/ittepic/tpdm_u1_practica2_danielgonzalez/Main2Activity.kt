a1fc37587ec715a7dc598051a2e47829da0765c4
Daniel Arturo Gonzalez Placito <daargonzalezpl@ittepic.edu.mx>
2019-09-17 00:30:21 -0600

    Mejorarda Validacion

--- app/src/main/java/mx/edu/ittepic/tpdm_u1_practica2_danielgonzalez/Main2Activity.kt
+++ app/src/main/java/mx/edu/ittepic/tpdm_u1_practica2_danielgonzalez/Main2Activity.kt
@@ -17,6 +17,7 @@ class Main2Activity : AppCompatActivity() {
     var btn_Regresar: Button? = null
     var gridlayout: GridLayout? = null
     var btn_Capturar: Button? = null
+    var btn_Archivar: Button?=null
     var valor: EditText? = null
     var posicionC: EditText? = null
     var posicionR: EditText? = null
@@ -24,6 +25,8 @@ class Main2Activity : AppCompatActivity() {
     var archivo2: RadioButton? = null
     var archivo3: RadioButton? = null
     var cadena = ""
+    var m=0
+    var v=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +36,7 @@ class Main2Activity : AppCompatActivity() {
         btn_Regresar = findViewById(R.id.btnRegresar)
         gridlayout = findViewById(R.id.Gridlayoutxd)
         btn_Capturar = findViewById(R.id.btnCapturar)
+        btn_Archivar = findViewById(R.id.btnArchivar)
         valor = findViewById(R.id.valor)
         posicionC = findViewById(R.id.posicionC_layout)
         posicionR = findViewById(R.id.posicionR_layout)
@@ -41,24 +45,99 @@ class Main2Activity : AppCompatActivity() {
         archivo3 = findViewById(R.id.rb_Archivo1)
 
         var eligioVector = intent.extras?.getString("hide")
-        if (eligioVector?.equals("oculto") == true) {
+        if (eligioVector?.equals("oculta") == true) {
             layo_PR?.setVisibility(View.GONE)
+            var campo1 = intent.extras?.getString("campo1")
+            gridlayout?.columnCount = campo1.toString().toInt()
+        } else {
+            var campo1 = intent.extras?.getString("campo1")
+            var campo2 = intent.extras?.getString("campo2")
+            gridlayout?.columnCount = campo1.toString().toInt()
+            gridlayout?.rowCount = campo2.toString().toInt()
         }
-
-
 
         btn_Capturar?.setOnClickListener {
-            var numero = valor?.text.toString().toInt()
-            var columna = posicionC?.text.toString()
-            var renglon = posicionR?.text.toString()
-
-            gridlayout?.columnCount = columna.toInt()
+            if (eligioVector?.equals("oculta") == true) {
+                if (valor?.text.toString().isEmpty() == true || posicionC?.text.toString().isEmpty() == true) {
+                    val alerta = AlertDialog.Builder(this)
+                    alerta.setTitle("ERROR!!").setMessage("DEBES DE INTRODUCIR LO DE UN VECTOR")
+                        .show()
+                } else {
+                    var valor = valor?.text.toString()
+                    var numero = TextView(this)
+                    numero.setText(valor)
+                    gridlayout?.addView(numero)
+                }
+            }
+                else{
+                    if (valor?.text.toString().isEmpty() == true || posicionC?.text.toString().isEmpty() == true || posicionR?.text.toString().isEmpty() == true) {
+                        val alerta = AlertDialog.Builder(this)
+                        alerta.setTitle("ERROR!!")
+                            .setMessage("DEBES DE INTRODUCIR LO DE UNA MATRIZ")
+                            .show()
+                    } else {
+                        var valor = valor?.text.toString()
+                        var numero = TextView(this)
+                        numero.setText(valor)
+                        gridlayout?.addView(numero)
+                    }
+                }
         }
-
         //Accion Del Boton Regresar
         btn_Regresar?.setOnClickListener {
             finish()
         }
+      //Para Archivar
+        btn_Archivar?.setOnClickListener {
+            if(archivo1?.isChecked==true){
+                guardarArchivo1()
+            }
+            if(archivo2?.isChecked==true){
+                guardarArchivo2()
+            }
+            if(archivo3?.isChecked==true){
+                guardarArchivo3()
+            }
+        }
+    }
+    fun guardarArchivo1(){
+        cadena="V"+posicionC?.text.toString()+" /nValor: "+valor?.text.toString()
+        if(m==1){cadena= "Titulo: "+"M"+posicionC?.text.toString()+posicionR?.text.toString()+" /nValor: "+valor?.text.toString()}
+        val guardarArchivo=OutputStreamWriter(openFileOutput("archivo1.txt",Activity.MODE_PRIVATE))
+        guardarArchivo.write(cadena)
+        guardarArchivo.flush()
+        AlertDialog.Builder(this).setTitle("ATENCION")
+            .setMessage("SE GUARDO EN ARCHIVO 1")
+            .setPositiveButton("ACEPTAR"){dialog,which->}
+            .show()
+    }
+
+    fun guardarArchivo2(){
+        cadena="V"+posicionC?.text.toString()+" /nValor: "+valor?.text.toString()
+        if(m==1){cadena= "Titulo: "+"M"+posicionC?.text.toString()+posicionR?.text.toString()+" /nValor: "+valor?.text.toString()}
+        val guardarArchivo=OutputStreamWriter(openFileOutput("archivo2.txt",Activity.MODE_PRIVATE))
+        guardarArchivo.write("Titulo: "+cadena+" /nValor: "+valor?.text.toString())
+        guardarArchivo.flush()
+        AlertDialog.Builder(this).setTitle("ATENCION")
+            .setMessage("SE GUARDO EN ARCHIVO 2")
+            .setPositiveButton("ACEPTAR"){dialog,which->}
+            .show()
+
+    }
+    fun guardarArchivo3(){
+        cadena="V"+posicionC?.text.toString()+" /nValor: "+valor?.text.toString()
+        if(m==1){cadena= "Titulo: "+"M"+posicionC?.text.toString()+posicionR?.text.toString()+" /nValor: "+valor?.text.toString()}
+
+        val guardarArchivo=OutputStreamWriter(openFileOutput("archivo3.txt",Activity.MODE_PRIVATE))
+        guardarArchivo.write(cadena)
+
+        guardarArchivo.flush()
+        AlertDialog.Builder(this).setTitle("ATENCION")
+            .setMessage("SE GUARDO EN ARCHIVO 3")
+            .setPositiveButton("ACEPTAR"){dialog,which->}
+            .show()
+
     }
 }
+
 
